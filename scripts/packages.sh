066ede65e6d1e47aae07de252884c284da2b2287
Matt Egger <matt.egger@gmail.com>
2025-08-05 17:35:17 -0400

    install some missing sets

---
+++ scripts/packages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+slackpkg -default_answer=yes -batch=on install a
+slackpkg -default_answer=yes -batch=on install ap
+slackpkg -default_answer=yes -batch=on install d
+#slackpkg -default_answer=yes -batch=on install e
+#slackpkg -default_answer=yes -batch=on install f
+slackpkg -default_answer=yes -batch=on install k
+#slackpkg -default_answer=yes -batch=on install kde
+slackpkg -default_answer=yes -batch=on install l
+slackpkg -default_answer=yes -batch=on install n
+#slackpkg -default_answer=yes -batch=on install t
+slackpkg -default_answer=yes -batch=on install tcl
+#slackpkg -default_answer=yes -batch=on install x
+#slackpkg -default_answer=yes -batch=on install xap
+#slackpkg -default_answer=yes -batch=on install y
