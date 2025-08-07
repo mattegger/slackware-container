c9b5ab4e2ccd137f761ff3f5e95cb0430b903b86
Matt Egger <matt.egger@gmail.com>
2025-08-07 17:07:36 -0400

    unnecessary

--- scripts/import-slack-gpg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget --no-check-certificate -O /tmp/gpgkey https://www.slackware.com/infra/keys/GPG-KEY
-mkdir -p ~/.gnupg
-#gpg --yes --batch --delete-key "$SLACKKEY" &>/dev/null
-gpg --import /tmp/gpgkey &>/dev/null && \
-echo -e "\t\t\tSlackware Linux Project's GPG key added"
